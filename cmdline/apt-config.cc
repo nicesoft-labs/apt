362bcab37b5838d09ec86a47d140b68937d9dc42
nicesoft-labs <me@sbelikov.ru>
2025-12-29 20:40:10 +0300

    Update apt-config.cc

--- cmdline/apt-config.cc
+++ cmdline/apt-config.cc
@@ -1,21 +1,21 @@
 // -*- mode: c++; mode: fold -*-
-// Description								/*{{{*/
+// Описание                                                            /*{{{*/
 // $Id: apt-config.cc,v 1.5 2003/01/29 18:43:48 niemeyer Exp $
 /* ######################################################################
-   
+
    APT Config - Program to manipulate APT configuration files
-   
+
    This program will parse a config file and then do something with it.
-   
+
    Commands:
      shell - Shell mode. After this a series of word pairs should occure.
              The first is the environment var to set and the second is
-             the key to set it from. Use like: 
+             the key to set it from. Use like:
  eval `apt-config shell QMode apt::QMode`
-   
+
    ##################################################################### */
-									/*}}}*/
-// Include Files							/*{{{*/
+/*}}}*/
+// Подключаемые файлы                                                   /*{{{*/
 #include <apt-pkg/cmndline.h>
 #include <apt-pkg/error.h>
 #include <apt-pkg/init.h>
@@ -25,55 +25,69 @@
 #include <apti18n.h>
 
 // CNC:2003-02-14 - apti18n.h includes libintl.h which includes locale.h,
-// 		    as reported by Radu Greab.
-//#include <locale.h>
+//                  as reported by Radu Greab.
+// В C++ корректнее подключить <clocale>, чтобы setlocale был объявлен явно.
+#include <clocale>
+
+#include <cstring>   // strlen
 #include <iostream>
 #include <string>
-									/*}}}*/
+/*}}}*/
+
 using namespace std;
 
-// DoShell - Handle the shell command					/*{{{*/
+// DoShell - обработка команды shell                                     /*{{{*/
 // ---------------------------------------------------------------------
 /* */
 bool DoShell(CommandLine &CmdL)
 {
    for (const char **I = CmdL.FileList + 1; *I != 0; I += 2)
    {
-      if (I[1] == 0 || strlen(I[1]) == 0)
-	 return _error->Error(_("Arguments not in pairs"));
+      // Аргументы должны идти парами: <ENV_VAR> <config-key>
+      // (Строку ошибки оставляем английской: msgid должен быть стабильным)
+      if (I[1] == 0 || std::strlen(I[1]) == 0)
+         return _error->Error(_("Arguments not in pairs"));
 
       string key = I[1];
-      if (key.end()[-1] == '/') // old directory format
-	 key.append("d");
+
+      // Старый формат директорий: ключ заканчивается на '/'.
+      // Историческое поведение: добавляем 'd'.
+      if (!key.empty() && key[key.size() - 1] == '/')
+         key.append("d");
 
       if (_config->ExistsAny(key.c_str()))
-	 cout << *I << "='" << 
-	         SubstVar(_config->FindAny(key.c_str()),"'","'\\''") << '\'' << endl;
-      
+      {
+         cout << *I << "='"
+              << SubstVar(_config->FindAny(key.c_str()), "'", "'\\''")
+              << '\'' << endl;
+      }
    }
-   
+
    return true;
 }
-									/*}}}*/
-// DoDump - Dump the configuration space				/*{{{*/
+/*}}}*/
+
+// DoDump - вывести пространство конфигурации                             /*{{{*/
 // ---------------------------------------------------------------------
 /* */
 bool DoDump(CommandLine &CmdL)
 {
+   (void)CmdL; // аргумент не используется, но сигнатуру не меняем
    _config->Dump(cout);
    return true;
 }
-									/*}}}*/
-// ShowHelp - Show the help screen					/*{{{*/
+/*}}}*/
+
+// ShowHelp - показать справку                                           /*{{{*/
 // ---------------------------------------------------------------------
 /* */
 int ShowHelp()
 {
    ioprintf(cout,_("%s %s for %s %s compiled on %s %s\n"),PACKAGE,VERSION,
-	    COMMON_OS,COMMON_CPU,__DATE__,__TIME__);
+            COMMON_OS,COMMON_CPU,__DATE__,__TIME__);
    if (_config->FindB("version") == true)
       return 0;
-   
+
    cout <<
     _("Usage: apt-config [options] command\n"
       "\n"
@@ -84,13 +98,13 @@ int ShowHelp()
       "   dump - Show the configuration\n"
       "\n"
       "Options:\n"
-      "  -h   This help text.\n" 
-      "  -c=? Read this configuration file\n" 
+      "  -h   This help text.\n"
+      "  -c=? Read this configuration file\n"
       "  -o=? Set an arbitary configuration option, eg -o dir::cache=/tmp\n"
       "NiceSOFT LLC: ООО \"НАЙС СОФТ ГРУПП\" 5024245440 <niceos@ncsgp.ru>\n");
    return 0;
 }
-									/*}}}*/
+/*}}}*/
 
 int main(int argc,const char *argv[])
 {
@@ -104,11 +118,11 @@ int main(int argc,const char *argv[])
                                    {"dump",&DoDump},
                                    {0,0}};
 
-   // Set up gettext support
+   // Включаем gettext (переводы берутся из *.mo по textdomain(PACKAGE))
    setlocale(LC_ALL,"");
    textdomain(PACKAGE);
 
-   // Parse the command line and initialize the package library
+   // Разбор аргументов и инициализация подсистем APT
    CommandLine CmdL(Args,_config);
    if (pkgInitConfig(*_config) == false ||
        CmdL.Parse(argc,argv) == false ||
@@ -118,21 +132,21 @@ int main(int argc,const char *argv[])
       return 100;
    }
 
-   // See if the help should be shown
+   // Показать справку, если попросили или команда не указана
    if (_config->FindB("help") == true ||
        CmdL.FileSize() == 0)
       return ShowHelp();
 
-   // Match the operation
+   // Выполнить команду
    CmdL.DispatchArg(Cmds);
-   
-   // Print any errors or warnings found during parsing
+
+   // Печать ошибок/предупреждений
    if (_error->empty() == false)
    {
       bool Errors = _error->PendingError();
       _error->DumpErrors();
       return Errors == true?100:0;
    }
-   
+
    return 0;
 }
